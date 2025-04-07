a8439f0b1c9a41b762a3a234d1307c206c0a0c0a
Dan-A2 <danialataee1381@gmail.com>
2025-04-07 19:02:34 +0330

    cleaned util

--- Config/util.py
+++ Config/util.py
@@ -76,110 +76,10 @@ def is_consumption(df, target_column, label_column, threshold=0.5):
     return True
 
 
-def find_highest_cosine_distance(df, label_column):
-    """
-    This function finds the two columns with the highest cosine distance
-    and the corresponding value that caused it.
-    """
-    # Initialize variables to keep track of the results
-    highest_distance = 0
-    col = None
-    causing_value = None
-    
-    second_highest_distance = 0
-    second_col = None
-    second_causing_value = None
-
-    object_cols = df.select_dtypes(include=['object']).columns.tolist()
-    if label_column in object_cols:
-        object_cols.remove(label_column)
-
-    numeric_cols = df.select_dtypes(include=[np.number]).columns.tolist()
-    if label_column in numeric_cols:
-        numeric_cols.remove(label_column)
-
-    scaler = StandardScaler()
-
-    # Loop through all combinations of categorical columns
-    for target_column in object_cols:
-        # Extract unique values from the target column
-        unique_values = df[target_column].unique()
-
-        for value in unique_values:
-            subset_without_value = df[df[target_column] != value][numeric_cols]
-            subset_with_value = df[df[target_column] == value][numeric_cols]
-            subset_without_value_scaled = scaler.fit_transform(subset_without_value)
-            subset_with_value_scaled = scaler.fit_transform(subset_with_value)
-            pca_without_value = PCA(n_components=1)
-            pca_with_value = PCA(n_components=1)
-            pca_without_value.fit(subset_without_value_scaled)
-            pca_with_value.fit(subset_with_value_scaled)
-            eigenvector_without_value = pca_without_value.components_[0]
-            eigenvector_with_value = pca_with_value.components_[0]
-            cosine_similarity = np.dot(eigenvector_without_value, eigenvector_with_value) / (
-                np.linalg.norm(eigenvector_without_value) * np.linalg.norm(eigenvector_with_value)
-            )
-            cosine_distance = 1 - cosine_similarity
-            
-            if cosine_distance > highest_distance:
-                highest_distance = cosine_distance
-                col = target_column
-                causing_value = value
-            
-            elif highest_distance > cosine_distance > second_highest_distance and col != None and target_column != col[0]:
-                second_highest_distance = cosine_distance
-                second_col = target_column
-                second_causing_value = value
-
-    return {
-        'columns': (col, second_col),
-        'highest_cosine_distance': (highest_distance, second_highest_distance),
-        'causing_value': (causing_value, second_causing_value)
-    }
-
-
 def drop_redundant_cols(df):
     for col in df.columns:
         if len(df[col].unique()) == 1:
             df.drop(col,inplace=True,axis=1)
-
-
-def divide_dataframe_by_causing_values(df, result):
-    """
-    Divides the dataframe into 4 parts based on causing values of the highest and second-highest cosine distances.
-
-    Parameters:
-    - df: Original dataframe
-    - result: Output from find_highest_cosine_distance function
-
-    Returns:
-    - A dictionary containing the 4 dataframes
-    """
-    col1, col2 = result['columns']
-    value1, value2 = result['causing_value']
-    col1 = col1 + "_" + value1
-    col2 = col2 + "_" + value2
-
-    mask1 = df[col1] == True
-    mask2 = df[col2] == True
-
-    df_1 = df[mask1 & mask2]
-    df_2 = df[mask1 & ~mask2]
-    df_3 = df[~mask1 & mask2]
-    df_4 = df[~mask1 & ~mask2]
-
-    return df_1, df_2, df_3, df_4
-    
-
-
-def combine_pca_mca(pca_values, mca_values, pca_weight=0.5, mca_weight=0.5):
-    '''
-    The name is self-explanatory
-    '''
-    pca_normalized = (pca_values - np.min(pca_values)) / (np.max(pca_values) - np.min(pca_values))
-    mca_normalized = (mca_values - np.min(mca_values)) / (np.max(mca_values) - np.min(mca_values))
-    combined_scores = (pca_weight * pca_normalized) + (mca_weight * mca_normalized)
-    return combined_scores
 
 
 def create_pair_pca(df, pairs, target_col):
@@ -268,7 +168,7 @@ def create_pair_bradley(df, pairs, target_col):
 
 def create_pair_bradley_exp(df, pairs, target_col):
     '''
-    This function creates the dual dataframe used for our model according to the actual value with bradley-terry model
+    This function creates the dual dataframe used for our model according to the actual value with exponential bradley-terry model
     '''
     paired_data = []
     df_tmp = df.drop(columns=[target_col])
@@ -415,36 +315,6 @@ def pretrain_model_with_residuals(df, n_samples, pretrain_params, target_col, re
     return pretrained_model
 
 
-def pretrain_model_with_residuals_multi(dfs, n_samples, pretrain_params, target_col, residual_list, total_len):
-    """
-    Pretrains the model on the concatenation of all given dataframes using residuals.
-    
-    Parameters:
-    - dfs: List of dataframes to use for pretraining.
-    - n_samples: Number of samples to generate for training.
-    - pretrain_params: XGBoost training parameters.
-    - target_col: Target column name.
-    - residual_list: List of residuals corresponding to each dataframe.
-
-    Returns:
-    - pretrained XGBoost model.
-    """
-    all_pretrain_dfs = []
-    for i in range(len(dfs)):
-        df = dfs[i]
-        residuals = residual_list[i]
-        df.reset_index(drop=True, inplace=True)
-        pretrain_pairs = generate_weighted_pairs(df, n=(n_samples * len(df)) // total_len, residuals=residuals)
-        pretrain_df = create_pair_pca(df, pretrain_pairs, target_col)     
-        all_pretrain_dfs.append(pretrain_df)
-    final_pretrain_df = pd.concat(all_pretrain_dfs, ignore_index=True)
-    X_pretrain = final_pretrain_df.drop(columns=['label'])
-    y_pretrain = final_pretrain_df['label']
-    dtrain_pretrain = xgb.DMatrix(X_pretrain, label=y_pretrain, enable_categorical=True)
-    pretrained_model = xgb.train(pretrain_params, dtrain_pretrain, num_boost_round=XGB_ESTIMATORS)
-    return pretrained_model
-
-
 def generate_all_pairs(df):
     '''
     Generate all possible pairs of indices from the dataframe.
@@ -478,46 +348,6 @@ def select_most_uncertain_pairs(model, df, pairs, batch_size, target_col):
     selected_pairs = [pairs[i] for i in most_uncertain_indices]
     
     return selected_pairs
-
-
-def compare_finetuned_blank(df, test_df, pretrain_params, pretrained_model, target_col):
-    '''
-    This function compares the fine-tuned model with the blank model
-    '''
-    # Initialize lists to store F1 scores
-    f1_scores_pretrained = []
-    f1_scores_untrained = []
-
-    # Prepare the test data
-    X_test = test_df.drop(columns=[f'{target_col}_1', f'{target_col}_2', 'label'])
-    y_test = test_df['label']
-    dtest = xgb.DMatrix(X_test, enable_categorical=True)
-
-    for n in range(10, 501, 10):
-        # Generate n random pairs for fine-tuning
-        pairs_fine_tune = generate_random_pairs(df, n=n)
-        train_df_fine_tune = create_pair_pca(df, pairs_fine_tune, target_col)
-        
-        # Prepare the fine-tuning training data
-        X_train_fine_tune = train_df_fine_tune.drop(columns=['label'])
-        y_train_fine_tune = train_df_fine_tune['label']
-        dtrain_fine_tune = xgb.DMatrix(X_train_fine_tune, label=y_train_fine_tune, enable_categorical=True)
-        
-        # Fine-tune the pretrained model
-        finetuned_model = xgb.train(pretrain_params, dtrain_fine_tune, num_boost_round=XGB_ESTIMATORS, xgb_model=pretrained_model)
-        y_pred_finetuned = finetuned_model.predict(dtest)
-        y_pred_finetuned_binary = (y_pred_finetuned > 0.5).astype(int)
-        f1_finetuned = f1_score(y_test, y_pred_finetuned_binary)
-        f1_scores_pretrained.append(f1_finetuned)
-        
-        # Train the blank model from scratch on the same data
-        untrained_model = xgb.train(pretrain_params, dtrain_fine_tune, num_boost_round=XGB_ESTIMATORS)
-        y_pred_untrained = untrained_model.predict(dtest)
-        y_pred_untrained_binary = (y_pred_untrained > 0.5).astype(int)
-        f1_untrained = f1_score(y_test, y_pred_untrained_binary)
-        f1_scores_untrained.append(f1_untrained)
-    
-    return f1_scores_pretrained, f1_scores_untrained
 
 
 def uncertainty_blank(total_pairs, batch_size, all_pairs, df, target_col, add_noise, use_bradley, exp, noise, pretrain_params, dtest, y_test):
@@ -660,15 +490,3 @@ def calculate_pca_var(df, target_col_name, useless_cols=[]):
     # Calculate variance from residuals
     variance = np.mean(residuals**2)
     return variance, residuals
-
-
-def calculate_pca_var_multi(dfs, target_col_name, useless_cols=[]):
-    total_variance = 0
-    all_residuals = []
-
-    for df in dfs:
-        var, res = calculate_pca_var(df, target_col_name, useless_cols)
-        total_variance += var
-        all_residuals.append(res)
-
-    return total_variance, all_residuals  # all_residuals is now a 1D list
